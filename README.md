31a34e1a62acd5545a562dce12f11517b84c2e35
Lucas Pampolha <lpampolha@outlook.com>
2021-07-21 15:20:02 -0300

    estilização

---
+++ README.md
@@ -0,0 +1,10 @@
+##Projeto de Cadastro de usuários para E-Commerce
+
+#Cadastro das seguintes informações de usuários
+
+-Cadastro de Nome;
+-Cadastro de Login;
+-Cadastro de Senha;
+-Cadastro de E-mail;
+-Cadastro de Código de Produto;
+-Cadastro de Produto;
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,22 @@ function saveUser(){
   var inputMail = document.getElementById("email")
   var inputPass = document.getElementById("password")
   var inputLogin = document.getElementById("login")
+  var inputCode = document.getElementById("code")
+  var inputProduct = document.getElementById("product")
   
   var name = inputName.value
   var email = inputMail.value
   var password = inputPass.value
   var login = inputLogin.value
+  var code = inputCode.value
+  var product = inputProduct.value
 
   localStorage.setItem("nameSave", name)
   localStorage.setItem("mailSave", email)
   localStorage.setItem("passSave", password)
   localStorage.setItem("loginSave", login)
+  localStorage.setItem("codeSave", code)
+  localStorage.setItem("productSave", product)
 
   var span = document.getElementById("message")
   span.innerHTML = "Dados salvos com sucesso"
